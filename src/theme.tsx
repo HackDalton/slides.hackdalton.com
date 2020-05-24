b62d45096308c6dea78ac3d61a89d4bab3ad6564
William Barkoff <william@barkoffusa.com>
2020-05-23 20:02:57 -0400

    initial commit

---
+++ src/theme.tsx
@@ -0,0 +1,10 @@
+const theme = {
+	fonts: {
+		primary: '\'Lato\', sans-serif',
+		header: '\'Lato\', sans-serif',
+		paragraph: '\'Lato\', sans-serif',
+		text: '\'Lato\', sans-serif',
+	}
+};
+
+export default theme;
